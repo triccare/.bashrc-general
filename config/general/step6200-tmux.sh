1c5754acbbc6b3d3ec139866a34599f24f65e1a2
Jonathan Eisenhamer <eisenham@hecatean.stsci.edu>
2014-10-28 14:09:50 -0400

    Added attach command to attach to a tmux session.

--- config/general/step6200-tmux.sh
+++ config/general/step6200-tmux.sh
@@ -2,4 +2,30 @@
 function mktmux() {
     local name="`date +%Y%m%d`-`date +%k%M%S`"
     tmux new-session -s "$name"
-}; export mktmux
+}; export -f mktmux
+
+function attach() {
+    local session
+
+    alphabet=({a..z})
+    conflist=()
+
+    for session in `tmux list-session -F "#S"`; do
+        conflist+=("$session")
+    done
+
+    for ((index=0; index<${#conflist[@]}; index++)); do
+        echo -e "[${alphabet[$index]}] ${conflist[$index]}"
+    done
+
+    read -n 1 -p 'Attache to? ' session
+    echo
+
+    answerIndex=$(elementIndex $session alphabet)
+    if [ "$answerIndex" -gt "$index" ]; then
+        echo "Wrong answer! Nothing done for you!"
+    else
+        tmux attach -t ${conflist[$answerIndex]}
+    fi
+
+}; export -f attach
