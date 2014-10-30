539bb97ae3a9870f1447429c75bfe47155f1e7b0
triccare <triccare@gmail.com>
2014-10-30 16:48:29 -0400

    New command git_author to change git repo authors.

---
+++ config/general/step7000-git.sh
@@ -0,0 +1,32 @@
+# git configs
+function git_author() {
+
+    if `git rev-parse`; then
+        echo "Current author is `git config user.name` <`git config user.email`>"
+
+        local authors
+        declare -a authors
+        authors[0]="J.D.Eisenhamer:eisenhamer@stsci.edu"
+        authors[1]="triccare:triccare@gmail.com"
+
+        local alphabet author authorIndex index
+        alphabet=({a..z})
+        
+        for ((index=0; index<${#authors[@]}; index++)); do
+            echo -e "[${alphabet[$index]}] ${authors[$index]}"
+        done
+
+        read -n 1 -p 'git author? ' author
+        echo
+
+        authorIndex=$(elementIndex $author alphabet)
+        if [ "$authorIndex" -ge "$index" ]; then
+            echo "Invalid author! You are not an author!"
+        else
+            author=(${authors[authorIndex]//:/ })
+            git config user.name "${author[0]}" && \
+                git config user.email "${author[1]}" && \
+                echo "git repo author is `git config user.name` <`git config user.email`>"
+        fi
+    fi
+}; export of git_author
