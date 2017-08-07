295d93eb795e066de760b9e5cae8942c2382b1b8
triccare <triccare@gmail.com>
2017-08-07 15:24:43 -0400

    move osx specific options to the osx section

--- config/osx/step5000-basis.sh
+++ config/osx/step5000-basis.sh
@@ -2,3 +2,17 @@
 
 # Personal development
 export PERSONALDEVDIR=$HOME/Documents/projects
+
+# Unsetting things I don't want
+unset DYLD_LIBRARY_PATH
+
+# Catchall paths
+export PATH=/usr/local/bin:/usr/local/sbin:/opt/local/bin:/opt/local/sbin:$PATH
+export PATH=~/QtSDK/Desktop/Qt/474/gcc/bin:$PATH
+export PATH=~/bin:~/varnish/bin:~/varnish/sbin:$PATH
+
+export MANPATH=/opt/local/share/man:$MANPATH
+
+# Web development
+export ARCHFLAGS='-arch x86_64'
+export CFLAGS='-arch x86_64'
